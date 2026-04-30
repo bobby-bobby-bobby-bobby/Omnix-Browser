bb8f12c8dfec6e6b260314d2fe2e191b92150faa
Caleb Burns <caleb1313b@gmail.com>
2026-04-29 19:54:33 -0700

    Add setup script for Omnix Browser

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Setting up Omnix Browser..."
+
+npm install
+
+echo "Done. Engine is currently running in STUB mode."
+echo "Chromium integration layer is reserved for future implementation."
