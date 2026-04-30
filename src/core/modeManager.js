50fed34a1fd2d5ea092ac04aa7a292c27a634ec8
Caleb Burns <caleb1313b@gmail.com>
2026-04-29 19:49:43 -0700

    Add mode management functionality

--- src/core/modeManager.js
+++ src/core/modeManager.js
@@ -1,1 +1,14 @@
+export const MODES = {
+  STANDARD: "STANDARD",
+  PRIVATE: "PRIVATE" // Tor later
+};
 
+let currentMode = MODES.STANDARD;
+
+export function getMode() {
+  return currentMode;
+}
+
+export function setMode(mode) {
+  currentMode = mode;
+}
